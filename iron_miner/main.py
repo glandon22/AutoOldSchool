e72770b9447f621de0e4992dac1ba7a54cd093f7
George <missourimade@protonmail.com>
2021-12-28 21:45:03 -0600

    updating the iron miner script. it no longer works

--- iron_miner/main.py
+++ iron_miner/main.py
@@ -1,13 +1,17 @@
-#Script starting environment
-#compass due north
-#POV max top
-#max zoom
+# Script starting environment
+# compass due north
+# POV max top
+# max zoom
+# script doesn't really work anymore
 import datetime
 import keyboard as kb
-from general_utils import checkBag, randomSleep, showInvCoords
-from bezier import bezierMovement
+from PIL import Image
+import pyscreenshot as image_grab
+
+from osrs_utils import general_utils
 import pyautogui as agui
 import random
+
 oreMined = 0
 miningRock = 0
 rockCoords = [
@@ -15,52 +19,57 @@ rockCoords = [
     [1199, 1290, 400, 475],
     [1540, 1600, 744, 807]
 ]
+emptySlot = Image.open('.\\screens\\empty_slot.png')
+
+
+def check_bag():
+    iron_ore_coords = general_utils.showInvCoords(0)
+    im = image_grab.grab([iron_ore_coords[0], iron_ore_coords[2], iron_ore_coords[1], iron_ore_coords[3]])
+    return general_utils.calcImgDiff(im, emptySlot, 5)
+
+
 scriptStartTime = datetime.datetime.now()
 status = 'initing'
-#the script can click to fast before the ore has spawned, need to add in a fail safe counter
+# the script can click to fast before the ore has spawned, need to add in a fail-safe counter
 while oreMined < 2500:
     if status == 'initing':
         toRock = rockCoords[miningRock]
-        bezierMovement(toRock[0],toRock[1],toRock[2],toRock[3])
+        general_utils.bezierMovement(toRock[0], toRock[1], toRock[2], toRock[3])
         agui.click()
-        randomSleep(0.2,0.4)
-        toFirstInvSlot = showInvCoords(0)
-        bezierMovement(toFirstInvSlot[0],toFirstInvSlot[1],toFirstInvSlot[2],toFirstInvSlot[3])
+        general_utils.randomSleep(0.2, 0.4)
+        toFirstInvSlot = general_utils.showInvCoords(0)
+        general_utils.bezierMovement(toFirstInvSlot[0], toFirstInvSlot[1], toFirstInvSlot[2], toFirstInvSlot[3])
         status = 'mining'
         oreMined = oreMined + 1
         continue
 
-    isIronInBag = checkBag()
+    isIronInBag = check_bag()
     if isIronInBag:
         status = 'dropping ore'
         kb.press('shift')
-        randomSleep(0.2,0.4)
+        general_utils.randomSleep(0.2, 0.4)
         agui.click()
-        randomSleep(0.2,0.4)
+        general_utils.randomSleep(0.2, 0.4)
         kb.release('shift')
         toRock = rockCoords[oreMined % 3]
-        bezierMovement(toRock[0],toRock[1],toRock[2],toRock[3])
+        general_utils.bezierMovement(toRock[0], toRock[1], toRock[2], toRock[3])
         agui.click()
-        randomSleep(0.2,0.4)
-        toFirstInvSlot = showInvCoords(0)
-        bezierMovement(toFirstInvSlot[0],toFirstInvSlot[1],toFirstInvSlot[2],toFirstInvSlot[3])
+        general_utils.randomSleep(0.2, 0.4)
+        toFirstInvSlot = general_utils.showInvCoords(0)
+        general_utils.bezierMovement(toFirstInvSlot[0], toFirstInvSlot[1], toFirstInvSlot[2], toFirstInvSlot[3])
         status = 'mining'
         oreMined = oreMined + 1
-        #potential break
-        if random.randint(1,100) == 3:
+        # potential break
+        if random.randint(1, 100) == 3:
             print('sleeping for up to 4 seconds')
-            randomSleep(2.4,4.7)
-        elif random.randint(1,225) == 9:
-            randomSleep(10.7,16.9)
+            general_utils.randomSleep(2.4, 4.7)
+        elif random.randint(1, 225) == 9:
+            general_utils.randomSleep(10.7, 16.9)
             print('sleeping for up to 16 seconds')
-        elif random.randint(1,450) == 17:
+        elif random.randint(1, 450) == 17:
             print('sleeping for up to 78 seconds')
-            randomSleep(60.4,78.9)
+            general_utils.randomSleep(60.4, 78.9)
 
         print('runtime: ', datetime.datetime.now() - scriptStartTime)
         print('ore mined ', oreMined)
         print('xp gained ', oreMined * 35)
-
-
-
-
