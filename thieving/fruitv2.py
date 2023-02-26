452da4b45d798b42af142382fd3c094c84705f78
George <glandonwright@gmail.com>
2023-02-26 10:33:51 -0600

    fruit thief

--- thieving/fruitv2.py
+++ thieving/fruitv2.py
@@ -14,24 +14,19 @@ fruits = [
     19653,
     5972
 ]
+port = '56799'
+
 def main():
     start_time = datetime.datetime.now()
     while True:
-        start_time = general_utils.break_manager(start_time, 53, 58, 423, 551, 'pass_71', False)
-        q = {
-            'isFishing': True,
-            'inv': True,
-            'poseAnimation': True
-        }
-        data = general_utils.query_game_data(q)
-        if len(data["inv"]) == 28:
-            # salmon + trout = 335, 331
-            # shrimp anchovie 317, 321
-            # leaping trout 11328
-            general_utils.power_drop(data["inv"], [], fruits)
-        stall = general_utils.get_game_object('1801,3608,0', '28823', '56799')
+        start_time = general_utils.break_manager(start_time, 53, 58, 423, 551, 'julenth', False)
+        inv = general_utils.get_inv(port)
+        fruit_inv = general_utils.are_items_in_inventory_v2(inv, fruits)
+        if fruit_inv:
+            general_utils.power_drop(inv, [], fruits)
+        stall = general_utils.get_game_object('1801,3608,0', '28823', port)
         if stall:
             general_utils.move_and_click(stall['x'], stall['y'], 9, 9)
-            general_utils.random_sleep(0.4, 0.5)
+            general_utils.random_sleep(1.0, 1.4)
 
 main()
