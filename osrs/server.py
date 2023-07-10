8eedf383f302fb962c15f09c269e51c38ca7a0b5
George <glandonwright@gmail.com>
2023-07-10 14:02:24 -0500

    steam clean up

--- osrs/server.py
+++ osrs/server.py
@@ -28,10 +28,11 @@ def post_game_status(q, updated_config, port='56798'):
     while True:
         try:
             start = updated_config['timings']['break_start']
+            osrs.dev.app_log.info(f'Timing variables. Start: {start}')
             start = type(start) is datetime and f'{start.hour}:{start.minute}:{start.second}'
             end = updated_config['timings']['break_end']
-            end = type(end) is datetime and f'{end.hour}:{end.minute}:{end.second}'
             osrs.dev.app_log.info(f'Timing variables. Start: {start}, end: {end}')
+            end = type(end) is datetime and f'{end.hour}:{end.minute}:{end.second}'
             enhanced_q = {
                 'status': q,
                 'next_break': start,
