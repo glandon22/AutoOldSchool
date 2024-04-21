09a25ecbc815a6bac30ec0a5be42cf30888d51e2
George <glandonwright@gmail.com>
2024-04-21 12:22:49 -0500

    add combat script to runelite

---
+++ combat/kill_and_loot_v2.py
@@ -0,0 +1,84 @@
+import math
+import sys
+import pyautogui
+
+import osrs
+
+'''
+Arg list:
+NPC to kill as string
+potion to drink
+how often to pot up
+min health to eat at
+sys.argv[1]
+
+'''
+
+npc_to_kill = str(sys.argv[1]) or 'YAKS'
+pot = str(sys.argv[2]) or 'SC'
+pot_interval = int(sys.argv[3]) or 10
+min_health = int(sys.argv[4]) or 12
+
+print(npc_to_kill, pot, pot_interval, min_health)
+
+karambwan_id = '7946'
+
+
+def find_next_target(npcs):
+    res = False
+    for npc in npcs:
+        if npc['health'] != 0:
+            if not res or npc['dist'] < res['dist']:
+                res = npc
+    return res
+
+
+script_config = {
+    'intensity': 'high',
+    'login': False,
+    'logout': lambda: osrs.clock.random_sleep(11, 14),
+}
+
+
+def main():
+    qh = osrs.queryHelper.QueryHelper()
+    qh.set_npcs(npc_ids)
+    qh.set_skills({'hitpoints', 'strength', 'ranged', 'magic', 'attack', 'defence'})
+    qh.set_inventory()
+    qh.set_interating_with()
+    qh.set_widgets({'233,0'})
+    while True:
+        qh.query_backend()
+        if not qh.get_interating_with():
+            osrs.game.break_manager_v4(script_config)
+
+        if qh.get_skills('hitpoints')['boostedLevel'] < min_health:
+            k = qh.get_inventory(karambwan_id)
+            if not k:
+                exit('out of karambwans')
+            osrs.move.click(k)
+            osrs.clock.sleep_one_tick()
+        elif qh.get_interating_with():
+            print('In combat.')
+        else:
+            targ = find_next_target(qh.get_npcs())
+            if targ:
+                print(targ['health'])
+                osrs.move.fast_click(targ)
+
+        if qh.get_skills('strength')['boostedLevel'] - qh.get_skills('strength')['level'] < 5:
+            super_combat = osrs.inv.are_items_in_inventory_v2(qh.get_inventory(), [12695, 12697, 12699, 12701])
+            if not super_combat:
+                print('out of super combats')
+            else:
+                osrs.move.click(super_combat)
+
+        # check if i leveled
+        if qh.get_widgets('233,0'):
+            for i in range(3):
+                osrs.keeb.press_key('space')
+                osrs.clock.sleep_one_tick()
+
+
+
+main()
