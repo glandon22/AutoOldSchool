e34d74dd021bdc59e384f7154a0be025c179efae
George Wright <glandonwright@gmail.com>
2024-11-18 10:44:03 -0600

    l5

---
+++ leagues5/stash_locs.py
@@ -0,0 +1,65 @@
+## ZEAH
+
+soul_altar = [1811,3853] #
+mount_karuulm_north = [1304,3838] #
+arceuus_library = [1641,3807] #
+shayzien_war_tent = [1488,3637] #
+shayzien_combat_ring = [1541,3631] #
+hosidius_mess = [1645,3629] #
+hosidius_south = [1713,3469] # close bank
+
+## MORY
+
+# there is also one in the barrows chest room i need to find
+slayer_tower_top_floor = [3422,3535]
+canifis_center = [3491,3490] #
+mory_masoleum = [3501,3574] #
+mory_allotment = [3579,3526] # idk what this is mapped to yet
+castle_drakan = [3563,3379] #
+
+## Karamja
+
+musa_point = [2910,3168] #
+tai_bwo_wannai = [2801,3081] #
+shilo_bank = [2852,2952] #
+kharazi_jungle = [2966,2924] #
+tzhaar_weapons = [2479,5146] #
+tzhaar_gems = [2466,5150] #
+
+
+## Frem
+mountain_camp = [2811,3678] #
+troll_mountain = [2781,3785] #
+light_house = [2511,3640] #
+frem_isle_mine = [2376,3848]  #
+
+## Misthalin
+ge_entrance = [3161,3465] #
+varr_rune_shop = [3253,3404]
+draynor_market = [3083,3254]
+
+stash_lookup = {
+    'Soul Altar': soul_altar,
+    'Mount Karuulm': mount_karuulm_north,
+    'Shayzien War Tent': shayzien_war_tent,
+    'Shayzien Combat Ring': shayzien_combat_ring,
+    'Arceuus Library': arceuus_library,
+    'Hosidius Mess': hosidius_mess,
+    'Hosidius (Charcoal Burners)': hosidius_south,
+    'Canafis': canifis_center,
+    'Caste Drakan': castle_drakan,
+    'Morytania Masoleum': mory_masoleum,
+    'Kharazi Jungle': kharazi_jungle,
+    'Musa Point': musa_point,
+    'Shilo Village': shilo_bank,
+    'Tai Bwo Wannao Village': tai_bwo_wannai,
+    'Tzhaar Gem Stall': tzhaar_gems,
+    'Tzhaar Weapon Shop': tzhaar_weapons,
+    'Trollweist Mountain': troll_mountain,
+    'Fremennik Isles Mine': frem_isle_mine,
+    'Lighthouse': light_house,
+    'Mountain Camp': mountain_camp,
+    'Varrock (Grand Exchange)': ge_entrance,
+    'Varrock (Rune shop)': varr_rune_shop,
+    'Draynor Village (Market)': draynor_market,
+}
